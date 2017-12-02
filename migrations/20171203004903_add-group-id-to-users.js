cafbc39ae19479372a3182cee45f6bb27bf0a27a
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2017-12-03 00:54:07 +0800

    Added three migrations files and setup knexfile.js

---
+++ migrations/20171203004903_add-group-id-to-users.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('users',(table)=>{
+    table.integer('group_id').unsigned();
+    table.foreign('group_id').references('groups.id');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('users',(table)=>{
+    table.dropForeign('group_id');
+  })
+};
