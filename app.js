dc1613fd1d1d1d88767ed7af60f2860ec0484d81
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2017-12-03 01:12:27 +0800

    Added app.js with knex insert and query.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const knexfile = require('./knexfile')[process.env.NODE_ENV || 'development' ]
 const knex = require('knex')(knexfile)
 
+knex.insert({'name':'john.doe',
+            'email':'john.doe@gmail.com',
+            'created_at':new Date(),
+            'updated_at':new Date()}).into('users').then(()=>{
+    knex.select('*').from('users').then((users)=>{
+        users.forEach((user)=>{
+            console.log(user.name)
+        });
+    })
+});
